d4892b43321a41c9bc7e92e5960cefb2b8866ec3
nefi24 <nrodriguezpartida@byupathway.edu>
2026-03-09 22:59:15 -0500

    1st assingment

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+document.getElementById("lastModified").textContent =
+    "Last Modification: " + document.lastModified;
