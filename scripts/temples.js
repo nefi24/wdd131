8b5f2a97defa6ccd7230a7480ee74af0e84bc33b
nefi24 <nrodriguezpartida@byupathway.edu>
2026-03-17 20:39:21 -0500

    Menu fix

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,14 @@ document.getElementById("currentYear").textContent = currentYear;
 
 document.getElementById("lastModified").textContent =
     "Last Modification: " + document.lastModified;
+
+const menuBtn = document.getElementById("menuBtn");
+const primaryNav = document.getElementById("primaryNav");
+
+menuBtn.addEventListener("click", () => {
+    primaryNav.classList.toggle("open");
+
+    const isOpen = primaryNav.classList.contains("open");
+    menuBtn.setAttribute("aria-expanded", isOpen);
+    menuBtn.innerHTML = isOpen ? "&times;" : "&#9776;";
+});
